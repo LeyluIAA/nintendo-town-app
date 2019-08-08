8a40100716d7ae2f5f7db9a0aee479f629e47ec8
leyluiaa <leyluiaa@outlook.com>
2019-08-08 08:22:30 +0200

    add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/theme/main.scss'
 import '@/misc/register-service-worker'
 import '@/misc/handle-network-status'
 import '@/firebase/init'
@@ -10,6 +14,7 @@ import '@/firebase/authentication'
 import '@/misc/handle-apple-install-prompt'
 import 'pwacompat'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
