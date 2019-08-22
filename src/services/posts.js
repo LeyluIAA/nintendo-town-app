0c31c8e80d3be09a5d4ab661d76f3eb846d9c027
leyluiaa <leyluiaa@outlook.com>
2019-08-22 10:09:27 +0200

    request over https

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://www.nintendo-town.fr/wp-json/wp/v2'
+const API_URL = 'https://www.nintendo-town.fr/wp-json/wp/v2'
 
 export default {
   getAll(pageNumber) {
